ace07586b7e0f0f782b251b43b5631044aca542a
marketingromanov-tech <marketing.romanov@gmail.com>
2026-08-04 11:11:50 +0300

    Update dashboard period without page navigation

--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,7 @@ Route::middleware('guest')->group(function (): void {
 
 Route::middleware('auth')->group(function (): void {
     Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
+    Route::get('/dashboard-data', [DashboardController::class, 'data'])->name('dashboard.data');
     Route::get('/robots', RobotManager::class)->middleware('role:admin,operator,viewer')->name('robots.index');
     Route::get('/robots/{robot}', RobotDetail::class)->name('robots.show');
     Route::get('/users', UserManager::class)->middleware('role:admin')->name('users.index');
